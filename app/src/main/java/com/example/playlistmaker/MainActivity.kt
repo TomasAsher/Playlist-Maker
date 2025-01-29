d0cbf62f7c72600246d6ab556ed8c3f48cbd2a2f
TomasAsher <thetimurik@ya.ru>
2025-01-29 09:01:33 +0300

    Добавил тосты на кнопки

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,13 +1,54 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.search)
+        val buttonMedia = findViewById<Button>(R.id.media)
+        val buttonSettings = findViewById<Button>(R.id.settings)
+
+        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Страница Поиск пока в разработке",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        buttonSearch.setOnClickListener(buttonSearchClickListener)
+
+        val buttonMediaClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Страница Медиатека пока в разработке",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        buttonMedia.setOnClickListener(buttonMediaClickListener)
+
+        buttonSettings.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Страница Настройки пока в разработке",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -16,5 +57,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
+    } */
 }
